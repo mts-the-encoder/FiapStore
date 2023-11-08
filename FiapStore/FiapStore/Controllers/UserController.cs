b51920c7dbeb433798fdd51d4ebcaa7cc0c95ba3
matheus <89268454+mts-the-encoder@users.noreply.github.com>
2023-11-07 21:59:24 -0300

    first shit api version

---
+++ FiapStore/FiapStore/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using FiapStore.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FiapStore.Controllers;
+
+[ApiController]
+[Route("User")]
+public class UserController : ControllerBase
+{
+    private readonly IUserRepository _repository;
+    public UserController(IUserRepository repository)
+    {
+        _repository = repository;
+    }
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        return Ok("Success!");
+    }
+
+    [HttpGet("Id")]
+    public IActionResult GetById()
+    {
+        return Ok("Success by Id!");
+    }
+
+    [HttpPost]
+    public IActionResult Create()
+    {
+        return Created(string.Empty,"User Created!");
+    }
+
+    [HttpPut]
+    public IActionResult Update()
+    {
+        return Ok("User Updated!");
+    }
+
+    [HttpDelete]
+    public IActionResult Delete()
+    {
+        return Ok("User Deleted!");
+    }
+}
